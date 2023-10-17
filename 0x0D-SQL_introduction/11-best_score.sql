ba49b5e484115e4f5ca36c5f633e1fefd523b5ab
CTHartze <ismailmaho007@gmail.com>
2023-10-17 12:52:08 -0700

    Changed comment style 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
-/* Commands list all records in the table with a score >= 10 in current database in my MySQL server.
-Records sorted in descending score. */
+-- Commands list all records in the table with a score >= 10 in current database in my MySQL server.
+-- Records sorted in descending score.
 SELECT `score`, `name`
 FROM `second_table`
 WHERE `score` >= 10
